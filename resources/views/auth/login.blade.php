626237e8474f72cdf80915cfaa97b88b3f0e73d9
namakun <t.hiroki9211@gmail.com>
2024-12-07 01:32:02 +0900

    ログイン画面の日本語化と不要なwelcomeの削除

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,27 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex items-center justify-between mt-4">
+            <div>
+                <!-- Register Link -->
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('新規登録') }}
+                    </a>
+                @endif
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <div>
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('パスワードを忘れましたか？') }}
+                    </a>
+                @endif
+
+                <x-primary-button class="ms-3">
+                    {{ __('ログイン') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
